59d60836288f4c697e923d3fe777988134b821ba
Maciej Tomaszewski <mt.maciejtomaszewski@gmail.com>
2025-12-20 03:25:04 +0100

    feat: zmiany kosmetyczne pod windows

--- app/histogram_plot_dialog.py
+++ app/histogram_plot_dialog.py
@@ -55,14 +55,14 @@ class HistogramPlotDialog(QDialog):
         QPushButton {
             border-radius: 8px;
             padding: 4px 10px;
-            background-color: #525252;
+            background-color: #999999;
             border: 1px solid #a0a0a0;
         }
         QPushButton:hover {
-            background-color: #636363;
+            background-color: #aaaaaa;
         }
         QPushButton:pressed {
-            background-color: #c0c0c0;
+            background-color: #999999;
         }
         """)
         lut_button.clicked.connect(lambda: self.show_lut_tables(lut_data))
